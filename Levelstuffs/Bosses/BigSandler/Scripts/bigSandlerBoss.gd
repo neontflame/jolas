aa65a1faca4b486d5adb997d0d4a5b1604779dd0
charlie.bs <gallinabreno@gmail.com>
2026-05-24 15:54:34 -0300

    sandler real

--- Levelstuffs/Bosses/BigSandler/Scripts/bigSandlerBoss.gd
+++ Levelstuffs/Bosses/BigSandler/Scripts/bigSandlerBoss.gd
@@ -4,9 +4,14 @@ class_name BigSandler
 @export_category("Sandler Specific")
 @export var FUCKING_aura: AnimatedSprite2D
 @export var attack_timer: Timer
+@export var mini_sandler_timer: Timer
 @export var projectile_target_node: Node2D
+@export_group("Mini Sandler Position Markers")
+@export var left_limit: Marker2D
+@export var right_limit: Marker2D
 
 var click_remote_scene = preload("uid://di3ka5pvlhhe1")
+var mini_sandlers_scene = preload("uid://bicpxsxa0vmgb")
 
 enum attack_state {
 	NONE,
@@ -14,6 +19,8 @@ enum attack_state {
 	SANDLER_WAVE,
 	SANDLER_REVERT
 }
+
+signal click_clicked
 
 var current_attack_state: attack_state = attack_state.NONE:
 	set(v):
@@ -30,6 +37,9 @@ var current_attack_state: attack_state = attack_state.NONE:
 				attack_timer.start(3.0)
 			attack_state.SANDLER_REVERT:
 				leSprite.play('clickClick')
+				mini_sandler_timer.stop()
+				click_clicked.emit()
+				play_boss_sfx("hl_button_10", "BigSandler", 10.0)
 				attack_timer.start(3.0)
 
 #region Default Stuff
@@ -41,10 +51,14 @@ func _ready() -> void:
 
 func boss_signals() -> void:
 	attack_timer.timeout.connect(_on_attack_timer_timeout)
+	mini_sandler_timer.timeout.connect(spawn_mini_sandlers)
 	leSprite.frame_changed.connect(frame_specific_actions)
 
 func _physics_process(delta: float) -> void:
 	super(delta)
+	if is_on_floor():
+		if velocity.x != 0.0:
+			velocity.x = move_toward(velocity.x, 0.0, 200.0 * delta)
 #endregion
 
 #region Sandler Content
@@ -66,14 +80,34 @@ func frame_specific_actions():
 		"clickThrow":
 			match leSprite.frame:
 				1:
+					play_sfx("Thok")
 					throw_click_remote()
+		"clickStart":
+			match leSprite.frame:
+				5:
+					play_boss_sfx("hl_button_3", "BigSandler", 10.0)
+					mini_sandler_timer.start()
 
 func throw_click_remote():
 	var remote = click_remote_scene.instantiate() as BaseProjectile
 	projectile_target_node.add_child(remote)
 	remote.projectileOwner = self
-	remote.launch(aim_at_player(), 10.0)
+	remote.launch(aim_at_player(), 20.0)
 	remote.global_position = global_position
+
+func spawn_mini_sandlers():
+	var sander = mini_sandlers_scene.instantiate() as BaseProjectile
+	projectile_target_node.add_child(sander)
+	sander.projectileOwner = self
+	sander.launch(Vector2.DOWN, 10.0)
+	sander.global_position = Vector2(ms_get_random_x(), ms_get_random_y())
+	click_clicked.connect(sander.go_up)
+
+func ms_get_random_x() -> float:
+	return randf_range(left_limit.global_position.x, right_limit.global_position.x)
+	
+func ms_get_random_y() -> float:
+	return randf_range(left_limit.global_position.y, right_limit.global_position.y)
 
 func aim_at_player() -> Vector2:
 	return (GPStats.charObject.global_position - global_position).normalized()
--- Levelstuffs/Bosses/Boss.gd
+++ Levelstuffs/Bosses/Boss.gd
@@ -157,9 +157,9 @@ func play_sfx(name:String, volumeDB:float = 0.0):
 	sfx_player.volume_db = GeneralUtils.get_volume_db('sfx', volumeDB)
 	sfx_player.play()
 
-func play_mob_sfx(name:String, mob:String, volumeDB:float = 0.0):
+func play_boss_sfx(name:String, boss:String, volumeDB:float = 0.0):
 	if sfx_player.playing: sfx_player.stop()
-	sfx_player.stream = load("res://Levelstuffs/Mobs/" + mob + "/Sounds/" + name + ".ogg")
+	sfx_player.stream = load("res://Levelstuffs/Bosses/" + boss + "/Sounds/" + name + ".ogg")
 	sfx_player.volume_db = GeneralUtils.get_volume_db('sfx', volumeDB)
 	sfx_player.play()
 
